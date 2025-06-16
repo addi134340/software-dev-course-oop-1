8b57a06bd5ec7dea03e19ea598805c4e14fc5c67
Addison Fowler <fowler.addison13@gmail.com>
2025-06-16 03:49:26 -0500

    Completed Assignments

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -41,4 +41,12 @@ public class Student {
 
     // Add a void method called addAbsence that increments the daysAbsent field by 1.
     // Add a void method called addBonusPoints that takes a double parameter bonusPoints and adds it to the grade field.
+
+    public void addAbsence() {
+        daysAbsent++;
+    }
+
+    public void addBonusPoints(double bonusPoints) {
+        grade += bonusPoints;
+    }
 }
